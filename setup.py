d7028feb7c05123bd3646cfae791cff27a1dde05
Lukas Heidegger <lukas.heidegger@eon.com>
2024-03-15 15:30:18 +0100

    export environment

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
-    version="0.0.0",
-    description="Starter code.",
-    author="Student",
+    name="udacity-ml-ops-deployment",
+    version="0.0.1",
+    description="Udacity ML Ops nanodegree: deployment exercise. ",
+    author="LH",
 )
